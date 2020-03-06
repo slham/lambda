3364a8ecd282fd6a4af965a03f76730bfe563828
slhamptonii <s.l.hamptonii@gmail.com>
2020-03-05 22:32:03 -0800

    can git commit -am can

--- ss-bball-data-fetch/go.mod
+++ ss-bball-data-fetch/go.mod
@@ -5,5 +5,6 @@ go 1.13
 require (
 	github.com/aws/aws-lambda-go v1.14.1
 	github.com/aws/aws-sdk-go v1.29.17
-	github.com/google/uuid v1.1.1
+	github.com/google/uuid v1.1.1 // indirect
+	gopkg.in/yaml.v2 v2.2.2
 )
--- ss-bball-data-fetch/main.go
+++ ss-bball-data-fetch/main.go
@@ -3,22 +3,21 @@ package main
 import (
         "bytes"
         "context"
+        "encoding/json"
         "fmt"
         "github.com/aws/aws-lambda-go/lambda"
         "github.com/aws/aws-sdk-go/aws"
         "github.com/aws/aws-sdk-go/aws/session"
         "github.com/aws/aws-sdk-go/service/s3/s3manager"
+        "gopkg.in/yaml.v2"
         "io/ioutil"
         "net/http"
         "os"
         "time"
 )
 
-type MyEvent struct {
-        Name string `json:"name"`
-}
-
-func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
+//Handler to fetch nba player stats for the year 2020 and store as yaml file in S3
+func HandleRequest(ctx context.Context) (string, error) {
         bucket, nbaKey, nbaUrl := os.Getenv("BUCKET"), os.Getenv("NBA_API_KEY"), os.Getenv("NBA_API_URL")
 
         // Fetch player stat data
@@ -27,8 +26,21 @@ func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
                 return "failed to fetch nba player stat data", err
         }
 
+        //Read players json response
         defer res.Body.Close()
         body, err := ioutil.ReadAll(res.Body)
+
+        var players []Player
+        err = json.Unmarshal(body, &players)
+        if err != nil {
+        	return "failed to unmarshal player stat response", err
+        }
+
+        //Convert players to yaml
+        playersBytes, err := yaml.Marshal(players)
+        if err != nil {
+                return "failed to marshal player stats to yaml []byte", err
+        }
 
         // The session the S3 Uploader will use
         sess := session.Must(session.NewSession())
@@ -36,13 +48,13 @@ func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
         // Create an uploader with the session and default options
         uploader := s3manager.NewUploader(sess)
 
-        key := fmt.Sprintf("player-stats/2020/%d.json", time.Now().Unix())
+        key := fmt.Sprintf("player-stats/2020/%d.yaml", time.Now().Unix())
 
-        // Upload the file to S3.
+        // Upload the yaml file to S3.
         result, err := uploader.Upload(&s3manager.UploadInput{
                 Bucket: aws.String(bucket),
                 Key:    aws.String(key),
-                Body:   bytes.NewReader(body),
+                Body:   bytes.NewReader(playersBytes),
         })
         if err != nil {
                 return "failed to upload file, %v", err
@@ -53,4 +65,32 @@ func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
 
 func main() {
         lambda.Start(HandleRequest)
+}
+
+type Player struct {
+        Id              int       `json:"PlayerID" yaml:"id"`
+        TeamId          int       `json:"TeamID" yaml:"teamId,omitempty"`
+        Name            string    `json:"Name" yaml:"name"`
+        Position        string    `json:"Position" yaml:"pos,omitempty"`
+        Min             int       `json:"Minutes" yaml:"min,omitempty"`
+        Fgm             float32   `json:"FieldGoalsMade" yaml:"fgm,omitempty"`
+        Fga             float32   `json:"FieldGoalsAttempted" yaml:"fga,omitempty"`
+        Fgp             float32   `json:"FieldGoalsPercentage" yaml:"fgp,omitempty"`
+        Ftm             float32   `json:"FreeThrowsMade" yaml:"ftm,omitempty"`
+        Fta             float32   `json:"FreeThrowsAttempted" yaml:"fta,omitempty"`
+        Ftp             float32   `json:"FreeThrowsPercentage" yaml:"ftp,omitempty"`
+        Tpm             float32   `json:"ThreePointersMade" yaml:"tpm,omitempty"`
+        Tpa             float32   `json:"ThreePointersAttempted" yaml:"tpa,omitempty"`
+        Tpp             float32   `json:"ThreePointersPercentage" yaml:"tpp,omitempty"`
+        Reb             float32   `json:"TotalReboundsPercentage" yaml:"reb,omitempty"`
+        Ass             float32   `json:"AssistsPercentage" yaml:"ass,omitempty"`
+        Stl             float32   `json:"StealsPercentage" yaml:"stl,omitempty"`
+        Blk             float32   `json:"BlocksPercentage" yaml:"bks,omitempty"`
+        Tvs             float32   `json:"TurnOversPercentage" yaml:"tvs,omitempty"`
+        Dds             float32   `json:"DoubleDoubles" yaml:"dds,omitempty"`
+        Pts             float32   `json:"Points" yaml:"pts,omitempty"`
+        Gms             int       `json:"Games" yaml:"gms,omitempty"`
+        CreatedDateTime time.Time `yaml:"createdDateTime"`
+        UpdatedDateTime time.Time `yaml:"updatedDateTime"`
+        Score           float32   `yaml:"score"`
 }
