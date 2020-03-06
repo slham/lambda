c8fff17a6ac5216018a42d6a0a5c50b8d1d2042a
slhamptonii <s.l.hamptonii@gmail.com>
2020-03-05 22:15:51 -0800

    able to pull player data and store as json

--- ss-bball-data-fetch/main.go
+++ ss-bball-data-fetch/main.go
@@ -3,14 +3,15 @@ package main
 import (
         "bytes"
         "context"
-        "encoding/json"
         "fmt"
         "github.com/aws/aws-lambda-go/lambda"
         "github.com/aws/aws-sdk-go/aws"
         "github.com/aws/aws-sdk-go/aws/session"
         "github.com/aws/aws-sdk-go/service/s3/s3manager"
-        "github.com/google/uuid"
+        "io/ioutil"
+        "net/http"
         "os"
+        "time"
 )
 
 type MyEvent struct {
@@ -18,25 +19,30 @@ type MyEvent struct {
 }
 
 func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
-        bucket := os.Getenv("BUCKET")
-        sampleJsonString, err := json.Marshal(name)
+        bucket, nbaKey, nbaUrl := os.Getenv("BUCKET"), os.Getenv("NBA_API_KEY"), os.Getenv("NBA_API_URL")
+
+        // Fetch player stat data
+        res, err := http.Get( nbaUrl + "?key=" + nbaKey)
         if err != nil {
-                return "failed to marshal 'name'", err
+                return "failed to fetch nba player stat data", err
         }
+
+        defer res.Body.Close()
+        body, err := ioutil.ReadAll(res.Body)
+
         // The session the S3 Uploader will use
         sess := session.Must(session.NewSession())
 
         // Create an uploader with the session and default options
         uploader := s3manager.NewUploader(sess)
 
-        u := uuid.New()
-        key := fmt.Sprintf("responses/%s", u.String())
+        key := fmt.Sprintf("player-stats/2020/%d.json", time.Now().Unix())
 
         // Upload the file to S3.
         result, err := uploader.Upload(&s3manager.UploadInput{
                 Bucket: aws.String(bucket),
                 Key:    aws.String(key),
-                Body:   bytes.NewReader(sampleJsonString),
+                Body:   bytes.NewReader(body),
         })
         if err != nil {
                 return "failed to upload file, %v", err
